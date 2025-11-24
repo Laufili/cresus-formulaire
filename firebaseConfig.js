d1918849ec8fcd4672841e5b268b7296ad6ec390
Laufili <d.lauf@cresus-fondation.org>
2025-11-24 21:26:37 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,20 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDx4q5bN81Cr35rw4U8O7P9jhrMLNz3mio",
-  authDomain: "formulaire-cresus.firebaseapp.com",
-  projectId: "formulaire-cresus",
-  storageBucket: "formulaire-cresus.firebasestorage.app",   // ✔️ IMPORTANT
-  messagingSenderId: "72537941440",
-  appId: "1:72537941440:web:1d9d3331ae7510d7ea9425"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
